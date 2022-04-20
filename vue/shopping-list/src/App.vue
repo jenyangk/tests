313a35d7bb53fbb1dce9846cb50ee4bb22e1d366
Jen Yang Koh <jenyang@ualberta.ca>
2022-04-20 00:24:48 -0600

    Shopping List Init

---
+++ vue/shopping-list/src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="container">
+    <content-header title="Task Tracker"/>
+    <tasks-component :tasks="tasks"/>
+  </div>
+</template>
+
+<script>
+import ContentHeader from "./components/Header.vue";
+import TasksComponent from "./components/Tasks.vue"
+
+export default {
+  name: "App",
+  components: {
+    ContentHeader,
+    TasksComponent
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Appointment",
+        day: "March 1st at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Appointment",
+        day: "March 2st at 2:30pm",
+        reminder: true,
+      },
+    ];
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  color: #fff;
+  background: #323232;
+  font-family: "Poppins", sans-serif;
+}
+
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 3px solid white;
+  padding: 30px;
+  border-radius: 10px;
+}
+
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
